3e600aaae758127813fc3f1e373a3d3fdf6f59a9
nikitaromanoov <91135334+nikitaromanoov@users.noreply.github.com>
2023-03-28 14:31:33 +0300

    Add files via upload

---
+++ lab6/lab5-master/src/main/java/lab5/MyMutation.java
@@ -0,0 +1,44 @@
+package lab5;
+
+import org.uncommons.watchmaker.framework.EvolutionaryOperator;
+import org.uncommons.watchmaker.framework.FitnessEvaluator;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class MyMutation implements EvolutionaryOperator<double[]> {
+    public List<double[]> apply(List<double[]> population, Random random) {
+        // initial population
+        // need to change individuals, but not their number!
+        for (int i = 0; i < Math.floor(population.size()/8); i++) {
+            int index = ThreadLocalRandom.current().nextInt(0, population.size());
+            double[] news = population.get(index);
+            for (int j = 0; j < Math.floor(news.length/8); j++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(0, news.length);
+                double change = ThreadLocalRandom.current().nextDouble(-0.5, 0.5);
+                if (news[randomNum] + change > 5) {
+                    news[randomNum] = 5;
+                } else if (news[randomNum] + change < -5) {
+                    news[randomNum] = -5;
+                } else {
+                    news[randomNum] = news[randomNum] + change;
+                }
+            }
+            population.set(index, news);
+            //System.out.println(Arrays.toString(news));
+        }
+        for (int i = 0; i < Math.floor(population.size()/16); i++) {
+            int index = ThreadLocalRandom.current().nextInt(0, population.size());
+            double[] news = population.get(index);
+            for (int j = 0; j < Math.floor(news.length/25); j++) {
+                int randomNum = ThreadLocalRandom.current().nextInt(0, news.length);
+                news[randomNum] = ThreadLocalRandom.current().nextDouble(-5, 5);
+            }
+            population.set(index, news);
+        }
+
+        return population;
+    }
+}
